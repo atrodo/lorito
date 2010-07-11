f71ce7795e9204d7b8db08f46ec50c2ec56b5c31
Jon Gentle <atrodo@atrodo.org>
2010-07-10 20:24:40 -0400

    Document the mini pack format.
    Sadly, I mangled some nomenclature and called something a segment that
    isn't in the parrot world.  Will need to fix that sometime.  Sorry.

--- loader.c
+++ loader.c
@@ -1,3 +1,10 @@
 // This will load a lorito bytecode file into a lorito codeseg
 
-
+// Since this is temporary, and we'll end up throwing it away in favor of
+//  integrating with parrot's packfile format, this will be real simple.
+//
+// Integer: segment type (0 = code, 1 = data)
+//   Integer: Size of segement name
+//   String: segment name, null terminated
+//   Integer: Count (in 8 bytes, so a count of 1 == 8 bytes)
+//   Data
