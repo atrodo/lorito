684c9f159eb9b08eae3a3ac9a0f2a82bc627a6d2
Jon Gentle <atrodo@atrodo.org>
2010-07-15 22:11:59 -0400

    Add the float registers.

--- lorito.h
+++ lorito.h
@@ -77,6 +77,7 @@ typedef struct lorito_str_t Lorito_STR;
 struct lorito_reg_t
 {
   int regs_i[REGNUM];
+  double regs_f[REGNUM];
   struct lorito_pmc_t *regs_p[REGNUM];
   struct lorito_str_t *regs_s[REGNUM];
 };
