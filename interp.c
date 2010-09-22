2774ecf7cc9479db65e7dea978a483cf44d0aeae
Jon Gentle <atrodo@atrodo.org>
2010-09-21 22:18:30 -0400

    Infact had an error in the code for the smart intern string lookup.

--- interp.c
+++ interp.c
@@ -46,12 +46,12 @@ lorito_string(Lorito_Interp *interp, int length, const char *original)
   //printf("Lorito_Str: %s\n", original);
   // See if this char * has been entered before
   /*
+  */
   for (i = 0; i < interp->symbol_len; i++)
   {
-    if (*original == *(interp->symbols[i]->original))
+    if (original == (interp->symbols[i]->original))
       return interp->symbols[i];
   }
-  */
 
   if (length == 0)
   {
