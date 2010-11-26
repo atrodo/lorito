4a4fee1eb8c7ebcc2900420172d153cd022bf443
Jon Gentle <atrodo@atrodo.org>
2010-11-26 03:26:05 -0500

    Fill in and correct the code in pmc_func for size.

--- pmc_func.c
+++ pmc_func.c
@@ -5,10 +5,19 @@
 #include "pmc_func.h"
 #include "interp.h"
 
+void lorito_pmc_func(Lorito_Interp *interp, Lorito_Ctx *ctx, Lorito_Str *func)
+{
+  if (func == lorito_string(interp, 0, "size"))
+  {
+    lorito_pmc_func_size(interp, ctx);
+  }
+}
+
 void
 lorito_pmc_func_size(Lorito_Interp *interp, Lorito_Ctx *ctx)
 {
-  Lorito_PMC *source = lorito_pop_arg(ctx);
-  lorito_clr_arg(ctx);
-  lorit_push_arg(interp, ctx, source->size);
+  Lorito_PMC *source = lorito_pop_arg(interp, ctx);
+  lorito_clr_arg(interp, ctx);
+  Lorito_PMC *size = lorito_box_int_new(interp, source->size);
+  lorito_push_arg(interp, ctx, size);
 }
